43a276ff5f2084f525d7379b634c4f753a98ebea
Fabian Berressem <fberress@students.uni-mainz.de>
2021-04-10 14:55:48 +0200

    Adjusted and added __getitem__

--- data/util_torch.py
+++ data/util_torch.py
@@ -1,9 +1,15 @@
 """Base Dataset class."""
 from typing import Any, Callable, Dict, Sequence, Tuple, Union
 import torch
-
+from PIL import Image
+import numpy as np
+import random
 
 SequenceOrTensor = Union[Sequence, torch.Tensor]
+
+
+def load_img(fname):
+    return np.array(Image.open(fname))
 
 
 class BaseDataset(torch.utils.data.Dataset):
@@ -30,6 +36,7 @@ class BaseDataset(torch.utils.data.Dataset):
         targets: SequenceOrTensor,
         transform: Callable = None,
         target_transform: Callable = None,
+        return_eval: bool = False,
     ) -> None:
         if len(data) != len(targets):
             raise ValueError("Data and targets must be of equal length")
@@ -38,6 +45,7 @@ class BaseDataset(torch.utils.data.Dataset):
         self.targets = targets
         self.transform = transform
         self.target_transform = target_transform
+        self.return_elevation = return_eval
 
     def __len__(self) -> int:
         """Return length of the dataset."""
@@ -55,12 +63,28 @@ class BaseDataset(torch.utils.data.Dataset):
         -------
         (datum, target)
         """
-        datum, target = self.data[index], self.targets[index]
 
+        #datum, target = self.data[index], self.targets[index]
+        datum = load_img(self.data[index])
+        target = load_img(self.targets[index])
+
+        if self.return_elevation:
+            target = [target, load_img(self.targets[index].replace("label-chips", "eleva-chips"))]
+
+        # Adjusted trasnformations to follow https://github.com/pytorch/vision/issues/9#issuecomment-304224800
+        # This is necessary to allow for the same transformations of target and datum
+        seed = np.random.randint(2147483647)
+        random.seed(seed) # apply this seed to img tranfsorms
+        torch.manual_seed(seed) # needed for torchvision 0.7
         if self.transform is not None:
             datum = self.transform(datum)
 
+        random.seed(seed) # apply this seed to img tranfsorms
+        torch.manual_seed(seed) # needed for torchvision 0.7
         if self.target_transform is not None:
-            target = self.target_transform(target)
+            if self.return_elevation:
+                target = [self.target_transform(target[0]), self.target_transform(target[1])]
+            else:
+                target = self.target_transform(target)
 
         return datum, target
