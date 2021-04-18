9564f1ea64d7220e301b609dc9a5d46f375b0f77
Jones93 <Schaust.J@web.de>
2021-04-18 17:01:33 +0200

    added encoder freezing

--- models/unet.py
+++ models/unet.py
@@ -10,6 +10,7 @@ DECODER_USE_BATCHNORM = True
 ENCODER_NAME = "resnet18"
 ENCODER_WEIGHTS = "imagenet"
 ENCODER_DEPTH = 5
+FREEZE_ENCODER = False
 
 class Unet(smp.Unet):
     def __init__(
@@ -29,12 +30,21 @@ class Unet(smp.Unet):
         encoder_name = self.args.get("encoder_name", ENCODER_NAME)
         encoder_weights = self.args.get("encoder_weights", ENCODER_WEIGHTS)
         encoder_depth = self.args.get("encoder_depth", ENCODER_DEPTH)
+        freeze_encoder = self.args.get("freeze_encoder", FREEZE_ENCODER)
 
         self.padding = (math.ceil(self.image_size / 2**(encoder_depth))*2**(encoder_depth)-self.image_size)//2
 
         super().__init__(in_channels=self.in_channels, classes=self.num_classes, decoder_channels=decoder_channels,
                         decoder_use_batchnorm=decoder_use_batchnorm, encoder_name=encoder_name, encoder_weights=encoder_weights,
                         encoder_depth=encoder_depth)
+        if freeze_encoder:
+            self.freeze_encoder()
+
+
+    def freeze_encoder(self):
+        for name, param in self.named_parameters():
+            if "encoder" in name:
+                param.requires_grad = False
 
     def pad(self, x):
         return ZeroPad2d(self.padding)(x)
@@ -53,4 +63,5 @@ class Unet(smp.Unet):
         parser.add_argument("--encoder_name", type=str, default=ENCODER_NAME)
         parser.add_argument("--encoder_weights", type=str, default=ENCODER_WEIGHTS)
         parser.add_argument("--encoder_depth", type=int, default=ENCODER_DEPTH)
+        parser.add_argument("--freeze_encoder", action="store_true", default=False)
         return parser
