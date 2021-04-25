e0691e66e76797b4b528640508f85c74acf4c53f
Fabian Berressem <fberress@students.uni-mainz.de>
2021-04-25 13:38:16 +0200

    Moved functions from unet-lit-model to base-lit-model

--- lit_models/base.py
+++ lit_models/base.py
@@ -2,6 +2,12 @@ import argparse
 import pytorch_lightning as pl
 import torch
 import torchmetrics
+import numpy as np
+
+try:
+    import wandb
+except ModuleNotFoundError:
+    pass
 
 from pytorch_lightning.metrics.utils import to_onehot
 
@@ -136,6 +142,15 @@ class BaseLitModel(pl.LightningModule):  # pylint: disable=too-many-ancestors
         self.val_acc = Accuracy()
         self.test_acc = Accuracy()
 
+        self.train_iou = IoU(num_classes=self.num_classes, ignore_index = self.ignore_index)
+        self.val_iou = IoU(num_classes=self.num_classes, ignore_index = self.ignore_index)
+        self.test_iou = IoU(num_classes=self.num_classes, ignore_index = self.ignore_index)
+
+        self.train_f1 = F1(num_classes=self.num_classes)
+        self.val_f1 = F1(num_classes=self.num_classes)
+        self.test_f1 = F1(num_classes=self.num_classes)
+
+
     @staticmethod
     def add_to_argparse(parser):
         parser.add_argument("--optimizer", type=str, default=OPTIMIZER, help="optimizer class from torch.optim")
@@ -175,7 +190,11 @@ class BaseLitModel(pl.LightningModule):  # pylint: disable=too-many-ancestors
         if self.predict_elevation:
             loss += ALPHA_ELEVATION*self.elevation_loss(elevation, z)
 
-        self.log("train_loss", loss)
+        self.log("train_loss", loss, on_step=False, on_epoch=True)
+        self.train_iou(logits, y)
+        self.log("train_IoU", self.train_iou, on_step=False, on_epoch=True)
+        self.train_f1(logits, y)
+        self.log("train_f1", self.train_f1, on_step=False, on_epoch=True)
         self.train_acc(logits, y)
         self.log("train_acc", self.train_acc, on_step=False, on_epoch=True)
         return loss
@@ -192,18 +211,57 @@ class BaseLitModel(pl.LightningModule):  # pylint: disable=too-many-ancestors
         loss = self.calc_loss(logits, y)
         if self.predict_elevation:
             loss += ALPHA_ELEVATION*self.elevation_loss(elevation, z)
+
         self.log("val_loss", loss, prog_bar=True)
+        self.val_iou(logits, y)
+        self.log("val_iou", self.val_iou, on_step=False,
+                 on_epoch=True, prog_bar=True)
+        self.val_f1(logits, y)
+        self.log("val_f1", self.val_f1, on_step=False, on_epoch=True)
         self.val_acc(logits, y)
         self.log("val_acc", self.val_acc, on_step=False, on_epoch=True, prog_bar=True)
+        return {'x' : x[0].cpu().numpy(), 'y' : y[0].cpu().numpy(), 'logits' : logits[0].cpu()}
+
+    def validation_epoch_end(self, validation_step_outputs):
+        wandb_images = []
+        try:
+            for out in validation_step_outputs:
+
+                    original_image = np.moveaxis(out['x'], 0, -1)
+                    ground_truth_mask = out['y']
+                    # [7,300,300] -> [1,300,300] 1 soll dim argmax
+                    prediction_mask = torch.argmax(out['logits'], dim=0).numpy()
+                    wandb_image = wandb.Image(original_image, masks={
+                        "predictions": {
+                            "mask_data": prediction_mask,
+                            "class_labels": self.class_labels
+                        },
+                        "ground_truth": {
+                            "mask_data": ground_truth_mask,
+                            "class_labels": self.class_labels
+                        }
+                    })
+                    wandb_images.append(wandb_image)
+
+
+            self.logger.experiment.log({"predictions": wandb_images})
+        except AttributeError as e:
+                print(e)
+                pass
 
     def test_step(self, batch, batch_idx):  # pylint: disable=unused-argument
         if self.predict_elevation:
             x, y, z = batch
         else:
             x, y = batch
+
         logits = self(x)
         if self.predict_elevation:
             elevation = torch.squeeze(logits[:, self.num_classes:, :, :])
             logits = logits[:, :self.num_classes, :, :]
+        self.test_iou(logits, y)
+        self.log("test_iou", self.test_iou, on_step=False, on_epoch=True)
+        self.test_f1(logits, y)
+        self.log("test_f1", self.test_f1, on_step=False, on_epoch=True)
         self.test_acc(logits, y)
         self.log("test_acc", self.test_acc, on_step=False, on_epoch=True)
