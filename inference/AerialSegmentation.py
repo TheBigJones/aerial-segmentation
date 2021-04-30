f4ec290239bd0c1ca4d7ae2703c324257bfc6466
Jonathan Schaust <Schaust.J@web.de>
2021-04-30 12:54:04 +0000

    to_torchscript model for inference speed

--- inference/AerialSegmentation.py
+++ inference/AerialSegmentation.py
@@ -13,13 +13,14 @@ class SegModel:
             checkpoint_path=checkpoint_path, args=args, model=model
         )
     self.lit_model.eval()
+    self.scripted_model = self.lit_model.to_torchscript(method="script", file_path=None)
 
     @torch.no_grad()
     def predict(self, image):
         """
         Prediction function for benchmark prediciton at the end of the training process
         """
-        logits = self.lit_model(x)
+        logits = self.scripted_model(x)
         probs = F.softmax(logits, dim=1)
         pred = torch.topk(y_pred_softmax, 1)[1]
         
