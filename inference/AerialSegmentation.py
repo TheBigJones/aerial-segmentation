cb6d0b19e5e719a03a0cb5ca7e41a582c18b2662
Jonathan Schaust <Schaust.J@web.de>
2021-04-30 12:46:02 +0000

    Started adding inference functionality for wandb benchmark. Caching for saftey reasons!

---
+++ inference/AerialSegmentation.py
@@ -0,0 +1,26 @@
+from models.unet import Unet
+
+
+class SegModel:
+  """
+  Class to run inference at the end of testing
+  """
+
+  def __init__(self, lit_model, data_config, args):
+    data_config = data_config 
+    model = Unet(data_config=data_config, args=args)
+    self.lit_model = BaseLitModel.load_from_checkpoint(
+            checkpoint_path=checkpoint_path, args=args, model=model
+        )
+    self.lit_model.eval()
+
+    @torch.no_grad()
+    def predict(self, image):
+        """
+        Prediction function for benchmark prediciton at the end of the training process
+        """
+        logits = self.lit_model(x)
+        probs = F.softmax(logits, dim=1)
+        pred = torch.topk(y_pred_softmax, 1)[1]
+        
+        return pred
