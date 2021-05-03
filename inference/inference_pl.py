388028b2ccd18be2d2de42fa68e56dfab709a91a
Jones93 <Schaust.J@web.de>
2021-05-03 10:29:23 +0000

    safety commit as working in colab

--- inference/inference_pl.py
+++ inference/inference_pl.py
@@ -21,14 +21,11 @@ def category2mask(img):
 
     return mask
 
-def chips_from_image(img, size=300):
+def chips_from_image(img, size=300, stride=1):
     shape = img.shape
-
-    chip_count = math.ceil(shape[1] / size) * math.ceil(shape[0] / size)
-
     chips = []
-    for x in range(0, shape[1], size):
-        for y in range(0, shape[0], size):
+    for x in range(0, shape[1], int(size/stride)):
+        for y in range(0, shape[0], int(size/stride)):
             chip = img[y:y+size, x:x+size, :]
             y_pad = size - chip.shape[0]
             x_pad = size - chip.shape[1]
@@ -36,36 +33,44 @@ def chips_from_image(img, size=300):
             chips.append((chip, x, y))
     return chips
 
-def run_inference_on_file(imagefile, predsfile, model, transform, size=300, batchsize=64):
+def run_inference_on_file(imagefile, predsfile, model, transform, size=300, batchsize=64, stride=2):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
+    model.model.to(device)
+ 
     with Image.open(imagefile).convert('RGB') as img:
         nimg = np.array(Image.open(imagefile).convert('RGB'))
         shape = nimg.shape
-        chips = chips_from_image(nimg)
-
+        chips = chips_from_image(nimg, size=size, stride=stride)
+    
+    num_classes = model.model.num_classes + 1 
     chips = [(chip, xi, yi) for chip, xi, yi in chips if chip.sum() > 0]
-    prediction = np.zeros(shape[:2], dtype='uint8')
+    prediction = np.zeros((num_classes, shape[0], shape[1]), dtype='uint8')
     #inp = transform(np.transpose(np.array([chip for chip, _, _ in chips]), (0, 3, 1, 2)), np.zeros((len(chips), size, size)))
     #print(transform(np.transpose(np.array(chips[0]), (0, 1, 2)), np.zeros((size, size)))[0])
     #print("#"*20)
     inp = torch.stack([transform(np.transpose(np.array(chip), (0, 1, 2)), np.zeros((size, size)))[0] for chip, _, _ in chips])
-
-    print(inp.shape)
-
+    # print("Prediction Shape: ",prediction.shape)
+    # print("Input Shape: ",inp.shape)
     num_batches = (len(inp) + batchsize -1) // batchsize
 
     chip_preds_list = []
     for j in range(num_batches):
-        chip_preds_list.append(model.predict(inp[j*batchsize : min((j+1)*batchsize, len(inp))]))
+        batch_preds = model.predict(inp[j*batchsize : min((j+1)*batchsize, len(inp))].to(device))
+        ignores_tensor = torch.zeros(batchsize, 1, inp.shape[2], inp.shape[3])
+        batch_preds_with_ignore = torch.cat((ignores_tensor,batch_preds.to("cpu")), -3)
+        print(batch_preds_with_ignore.shape)
+        chip_preds_list.append(batch_preds_with_ignore)
 
     chip_preds = torch.cat(tuple(chip_preds_list))
 
-    print(chip_preds.shape)
+    # print("Chipped Predictions Shape: ",chip_preds.shape)
 
     for (chip, x, y), pred in zip(chips, chip_preds):
-        category_chip = np.argmax(pred, axis=-3) + 1
-        section = prediction[y:y+size, x:x+size].shape
-        prediction[y:y+size, x:x+size] = category_chip[:section[0], :section[1]]
-
+        # print("X: ", x, "/Y: ", y)
+        section = prediction[0, y:y+size, x:x+size].shape
+        # print("Section Shape", section)
+        prediction[:, y:y+size, x:x+size] = np.add(prediction[:, y:y+size, x:x+size], pred[:, :section[0], :section[1]])
+    prediction = np.argmax(prediction, axis=-3)
     mask = category2mask(prediction)
     Image.fromarray(mask).save(predsfile)
 
